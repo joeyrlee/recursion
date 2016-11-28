9e7e657e0a816ef974a66ac68ee3debdc4a05b17
Joey Lee <jleepianist@gmail.com>
2016-11-28 10:19:38 -0800

    Added copyObject helper function to duplicate object/array inputs

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,10 +2,23 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
+var copyObject = function(obj){
+  if(Array.isArray(obj)){
+  	return Array.prototype.slice.call(obj, 0);
+  } else if(typeof obj === 'object'){
+  	var objCopy = {};
+  	for(var key in obj){
+      objCopy[key] = obj[key];
+  	}
+  	return objCopy;
+  }
+}
+
 var stringifyJSON = function(obj) {
   //Covers everything but arrays/objects
   if(typeof obj === 'boolean'){
   	return `${obj}`;
+  //strict equals evaluation since typeof null doesn't work
   } else if(obj === null){
     return `${obj}`;
   } else if(typeof obj === 'number'){
@@ -17,4 +30,7 @@ var stringifyJSON = function(obj) {
   } else if(typeof obj === 'function'){
   	return undefined;
   }
+
+  var objCopy = copyObject(obj);
+  
 };
