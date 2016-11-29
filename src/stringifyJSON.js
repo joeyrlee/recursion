0fe0d750e808cdaa2b08181d39b3b908355815cc
Joey Lee <jleepianist@gmail.com>
2016-11-28 21:58:39 -0800

    Completed stringifyJSON (Finalized support for arrays/objects)

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,35 +2,63 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
+
+//copies arrays/objects so that originals aren't mutated in recursive stringification
 var copyObject = function(obj){
   if(Array.isArray(obj)){
-  	return Array.prototype.slice.call(obj, 0);
+    return Array.prototype.slice.call(obj, 0);
   } else if(typeof obj === 'object'){
-  	var objCopy = {};
-  	for(var key in obj){
+    var objCopy = {};
+    for(var key in obj){
       objCopy[key] = obj[key];
-  	}
-  	return objCopy;
+    }
+    return objCopy;
   }
 }
 
-var stringifyJSON = function(obj) {
+var stringifyJSON = function(obj){
   //Covers everything but arrays/objects
   if(typeof obj === 'boolean'){
-  	return `${obj}`;
+    return `${obj}`;
   //strict equals evaluation since typeof null doesn't work
   } else if(obj === null){
     return `${obj}`;
   } else if(typeof obj === 'number'){
     return `${obj}`;
   } else if(typeof obj === 'string'){
-  	return '"' + obj + '"';
+    return '"' + obj + '"';
   } else if(typeof obj === 'undefined'){
-    return undefined;
+    return '';
   } else if(typeof obj === 'function'){
-  	return undefined;
+    return '';
   }
 
-  var objCopy = copyObject(obj);
-  
-};
+  var stringifyArray = function(arr){
+    if(arr.length === 0){
+      return '';
+    } else {
+      return `${stringifyJSON(arr.shift())},` + stringifyArray(arr);
+    }
+  }
+
+  var copy = copyObject(obj);
+  if(Array.isArray(copy)){
+    return '[' + stringifyArray(copy).slice(0, -1) + ']';
+  } else {
+    var result = (function stringifyObj(){
+      if(Object.keys(copy).length === 0){
+        return '';
+      } else {
+        var key = Object.keys(copy)[0];
+        var value = Object.values(copy)[0];
+        delete copy[key];
+        if (key === 'functions' || key === 'undefined'){
+          return '' + stringifyObj();
+        } else {
+          return `${stringifyJSON(key)}:${stringifyJSON(value)},` + stringifyObj();
+        }
+      }
+    }());
+    return '{' + result.slice(0, -1) + '}';
+  }
+}
