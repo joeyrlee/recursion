c1679e1035c15c5130c398510385cb6b9b274a97
Joey Lee <jleepianist@gmail.com>
2016-12-08 12:06:13 -0800

    Wrote out code/pseudocode for function shell

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className){
+  //document.body, element.childNodes, element.classList
+   //.hasChildNodes, node.classList.contains(className)
+  
+  var elements = [];
+  var domRecursion = function(element){
+  	if(element.classList.contains(className)){
+  	  elements.push(element);
+  	}
+    for(var i = 0; i < element.childNodes.length; i++){
+   	  
+   	  if(element.childNode[i].classList.contains(className)){
+   	  	elements.push()
+   	  }
+      //check each childNode for the className using element.classList
+      //and then delve into each childNode for the className using element.classList
+      //and each childNode's childNode using element.classList
+	}
+  }
+
+  domRecursion(document.body);
+
+  return elements.length >= 1 || null;
 };
